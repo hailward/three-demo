bbd0e09f0eea4b6f3d520df46c761b9862414614
wuhao <hao.wu@vcg.com>
2021-08-09 12:05:58 +0800

    init

---
+++ src/pages/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>Home</header>
+  <main ref="container" />
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import * as three from "three";
+import * as dat from "dat.gui";
+export default {
+  setup() {
+    const container = ref(null);
+    onMounted(() => {
+      console.log(container.value);
+    });
+    const initGui = () => {
+      const gui = new dat.GUI({ name: "My GUI" });
+      gui.add({ rotation: 0 }, "rotation", 0, 360);
+    };
+    onMounted(() => {
+      initGui();
+    });
+    return {
+      container,
+    };
+  },
+};
+</script>
