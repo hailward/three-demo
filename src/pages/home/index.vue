91afc877f1411344de2bf4bf30465f29a5da40b7
wuhao <hao.wu@vcg.com>
2021-08-10 16:33:03 +0800

    home page finished

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,28 +1,197 @@
 <template>
-  <header>Home</header>
-  <main ref="container" />
+  <main class="main" ref="containerRef" />
+  <div class="status">
+    <div>
+      <p>Camera position</p>
+      <p>positionX: {{ cameraRef.position.x.toFixed(0) }}</p>
+      <p>positionY: {{ cameraRef.position.y.toFixed(0) }}</p>
+      <p>positionZ: {{ cameraRef.position.z.toFixed(0) }}</p>
+    </div>
+    <div>
+      <p>Camera rotation</p>
+      <p>
+        rotationX: {{ ((cameraRef.rotation.x / Math.PI) * 180).toFixed(0) }}°
+      </p>
+      <p>
+        rotationY: {{ ((cameraRef.rotation.y / Math.PI) * 180).toFixed(0) }}°
+      </p>
+      <p>
+        rotationZ: {{ ((cameraRef.rotation.z / Math.PI) * 180).toFixed(0) }}°
+      </p>
+    </div>
+  </div>
 </template>
 
-<script>
-import { ref, onMounted } from "vue";
-import * as three from "three";
-import * as dat from "dat.gui";
-export default {
+<script lang="ts">
+import {
+  ref,
+  shallowRef,
+  onMounted,
+  watchEffect,
+  defineComponent,
+} from "vue";
+import * as THREE from "three";
+import FontJSON from "three/examples/fonts/helvetiker_regular.typeface.json";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export default defineComponent({
   setup() {
-    const container = ref(null);
-    onMounted(() => {
-      console.log(container.value);
+    const containerRef = ref(null);
+    const instanceRef = shallowRef({
+      scene: null,
+      camera: null,
+      renderer: null,
     });
-    const initGui = () => {
-      const gui = new dat.GUI({ name: "My GUI" });
-      gui.add({ rotation: 0 }, "rotation", 0, 360);
+    const cameraRef = ref({
+      position: { x: 0, y: 0, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+    });
+    // initialize scene/camera/renderer
+    const initInstances = () => {
+      const container = containerRef.value;
+      const { offsetWidth, offsetHeight } = container;
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        offsetWidth / offsetHeight,
+        0.1,
+        1000
+      );
+      camera.position.set(0, 50, 100);
+      camera.lookAt(0, 0, 0);
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(offsetWidth, offsetHeight);
+      renderer.setClearColor(0x222842, 1);
+      container.appendChild(renderer.domElement);
+      instanceRef.value = { scene, camera, renderer };
+      interuptRef.value = false;
+      window.scene = scene;
+      window.camera = camera;
+      window.renderer = renderer;
+      cameraRef.value = { ...camera };
+    };
+    // initialize controls
+    const initControls = () => {
+      const { camera, renderer } = instanceRef.value;
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.addEventListener("change", () => {
+        cameraRef.value = { ...camera };
+      });
+      // 限制俯仰角
+      // controls.minPolarAngle = Math.PI * (60 / 180);
+      // controls.maxPolarAngle = Math.PI * (80 / 180);
+      // controls.maxPolarAngle = Math.PI * (90 / 180);
+      // 启用阻尼
+      // controls.enableDamping = true
+      // controls.dampingFactor = .01
+      // 监听按键
+      // controls.listenToKeyEvents(document.body);
+      // controls.keys = {
+      //   LEFT: "ArrowLeft", //left arrow
+      //   UP: "ArrowUp", // up arrow
+      //   RIGHT: "ArrowRight", // right arrow
+      //   BOTTOM: "ArrowDown", // down arrow
+      // };
+      // 监听鼠标
+      controls.mouseButtons = {
+        LEFT: THREE.MOUSE.ROTATE,
+        MIDDLE: THREE.MOUSE.DOLLY,
+        // RIGHT: THREE.MOUSE.PAN,
+      };
+      // 监听触摸
+      controls.touches = {
+        ONE: THREE.TOUCH.ROTATE,
+        TWO: THREE.TOUCH.DOLLY_PAN,
+      };
+    };
+    // initialize lights
+    const initLights = () => {
+      const { scene } = instanceRef.value;
+      const lights = [new THREE.AmbientLight(0xffffff, 0.5)];
+      scene.add(...lights);
+    };
+    // initialize renferences
+    const initReferences = () => {
+      const { scene } = instanceRef.value;
+      const gridWidth = 100;
+      const grid = new THREE.GridHelper(gridWidth, 10);
+      scene.add(grid);
+      const axes = new THREE.AxesHelper(gridWidth / 2 + 5);
+      scene.add(axes);
+      const createText = (text, color) => {
+        var geometry = new THREE.TextGeometry(text, {
+          font: new THREE.Font(FontJSON),
+          size: 5,
+          height: 0.5,
+          // curveSegments: 12,
+          // bevelEnabled: true,
+          // bevelThickness: 10,
+          // bevelSize: 8,
+          // bevelSegments: 5,
+        });
+        geometry.computeBoundingBox();
+        const material = new THREE.MeshPhysicalMaterial({ color });
+        const textObj = new THREE.Mesh(geometry, material);
+        textObj.position.x = -geometry.boundingBox.max.x / 2;
+        textObj.position.y = -geometry.boundingBox.max.y / 2;
+        textObj.position.z = -geometry.boundingBox.max.z / 2;
+        return textObj;
+      };
+      const textX = createText("X", 0xff0000);
+      textX.position.x = gridWidth / 2 + 6;
+      const textY = createText("Y", 0xffff00);
+      textY.position.y = gridWidth / 2 + 6;
+      const textZ = createText("Z", 0x0000ff);
+      const wrappedTextZ = new THREE.Group();
+      wrappedTextZ.add(textZ);
+      wrappedTextZ.position.z +=
+        gridWidth / 2 + 6 + textZ.geometry.boundingBox.max.y / 2;
+      wrappedTextZ.rotation.x = -Math.PI / 2;
+      scene.add(textX, textY, wrappedTextZ);
+    };
+    // render
+    const render = () => {
+      const { scene, camera, renderer } = instanceRef.value;
+      renderer.render(scene, camera);
+    };
+    // loop update
+    const interuptRef = ref(false);
+    const update = () => {
+      const interupt = interuptRef.value;
+      if (interupt) return;
+      requestAnimationFrame(update);
+      render();
     };
     onMounted(() => {
-      initGui();
+      watchEffect((onInvalidate) => {
+        initInstances();
+        initControls();
+        initLights();
+        initReferences();
+        update();
+        onInvalidate(() => {
+          interuptRef.value = true;
+        });
+      });
     });
     return {
-      container,
+      containerRef,
+      cameraRef,
     };
   },
-};
+});
 </script>
+
+<style lang="scss" scoped>
+.main {
+  width: 100vw;
+  height: 100vh;
+}
+.status {
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: #fff;
+  padding: 10px;
+}
+</style>
